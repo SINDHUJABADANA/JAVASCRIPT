5da75253b68abf3f819f5faa54dd00abaccd00f3
SINDHUJABADANA <97426085+SINDHUJABADANA@users.noreply.github.com>
2023-08-18 20:40:26 +0530

    JAVASCRIPT
    JAVASCRIPT

---
+++ introstyles.js
@@ -0,0 +1,5 @@
+var a=10;
+document.write(a);
+document.write(obj1[0].name+" "+obj1[0].rollno+" "+obj1[0].campus+" "+obj1[0].branch+"</br>");
+    document.write(obj1[1].name+" "+obj1[1].rollno+" "+obj1[1].campus+" "+obj1[1].branch+"</br>");
+    document.write(obj1[1]['name']+" "+obj1[1]['rollno']+" "+obj1[1]['campus']+" "+obj1[1]['branch']+"</br>");
